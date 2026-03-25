f90cb9b59da535ce2749d3eed765569f08830f71
douxxtech <douxx@douxx.tech>
2026-03-25 12:30:18 +0100

    Now logging requests into the file descriptor stored in "log_file" (populated by initialsetup)

--- program.asm
+++ program.asm
@@ -759,7 +759,8 @@ _start:
     PARSE_UA_HEADER      request, 8192, user_agent, 1024
     PARSE_REFERER_HEADER request, 8192, referer,    1024
 
-    LOG_REQUEST_CLFE
+    mov r9, qword [log_file]
+    LOG_REQUEST_CLFE r9
 
     add rsp, 16
     EXIT 0 ; child exits
